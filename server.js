534b0323fd9b6c8e0a7d3c6894e25c6dda1af883
Regita Dwi Ayu Lestari <lestariregitadwiayu@gmail.com>
2020-02-15 12:52:52 +0700

    Make it better

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var cors = require("cors")
+var cors = require("cors");
 var path = require('path');
 const app = express();
 
-// parse requests - application/json
+// parse requests -application/json
 app.use(bodyParser.json());
 
-// parse requests - application/x-www-form-urlencoded
+// parse requests -application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //cors
@@ -19,8 +19,9 @@ app.use(express.static(path.join(__dirname, "./public")));
 app.get("/", (req, res) => {
     res.json({ message: "Selamat datang pada matakuliah pemrograman perangkat bergerak" });
 });
+
 require("./app/routes/book.routes")(app);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
